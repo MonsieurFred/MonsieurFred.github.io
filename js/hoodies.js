2e99b76cc4bf7a735a90f252627318591195bc0d
MonsieurFred <82777222+MonsieurFred@users.noreply.github.com>
2021-04-21 14:23:20 +0200

    Add files via upload

---
+++ js/hoodies.js
@@ -0,0 +1,64 @@
+
+
+function ouvrirPage () {
+    var a = document.getElementById("search").value;
+
+    if (a === 't-shirt') {
+        window.open ("t-shirt.html",'_self')
+    }
+    if (a === 'sneakers') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'cap') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'hoodie') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'hoodies') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'pull') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'sweat') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'sweat-shirt') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'polo') {
+        window.open ("t-shirt.html",'_self')
+    }
+    if (a === 't-shirts') {
+        window.open ("t-shirt.html",'_self')
+    }
+    if (a === 'casquette') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'casquettes') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'chapeaux') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'chapeau') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'hat') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'chausures') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'chausure') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'basquette') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'basquettes') {
+        window.open ("sneakers.html",'_self')
+    }
+
+}
---
+++ js/index.js
@@ -0,0 +1,137 @@
+
+function ouvrirPage () {
+    var a = document.getElementById("search").value;
+
+    if (a === 't-shirt') {
+        window.open ("t-shirt.html",'_self')
+    }
+    if (a === 'sneakers') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'cap') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'hoodie') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'hoodies') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'pull') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'sweat') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'sweat-shirt') {
+        window.open ("hoodies.html",'_self')
+    }
+    if (a === 'polo') {
+        window.open ("t-shirt.html",'_self')
+    }
+    if (a === 't-shirts') {
+        window.open ("t-shirt.html",'_self')
+    }
+    if (a === 'casquette') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'casquettes') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'chapeaux') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'chapeau') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'hat') {
+        window.open ("caps.html",'_self')
+    }
+    if (a === 'chausures') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'chausure') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'basquette') {
+        window.open ("sneakers.html",'_self')
+    }
+    if (a === 'basquettes') {
+        window.open ("sneakers.html",'_self')
+    }
+
+}
+
+
+// var maVariable = 10;
+// var typeDeVariable = typeof maVariable;
+// alert (typeDeVariable)
+
+// var humeur = prompt("comment ca va ");
+// var prenom = prompt("comment t'appele-tu");
+// var phrase = 'tu t\'appelles ' + prenom + ' et tu va ' + humeur;
+// alert(phrase);
+
+
+// alert('bienvenu dans notre programe d addition');
+
+// var nombreUn = prompt ('votre premier nombre');
+// console.log =('nombre un' + nombreUn);
+
+
+// var nombreDeuc = prompt ('votre dexième nombre');
+// console.log = ('nombre deux ' +  'nombreDeuc');
+
+// console.log = ('opération en cours');
+// var resultat = Number(nombreUn) + Number(nombreDeuc);
+
+
+
+
+// console.log = ( "affichage du résultat" + resultat );
+// alert (resultat);
+
+
+
+// var chiffre1 = 10
+// var chiffre2 = 5
+// var one = chiffre1 == chiffre2 +5;
+// alert(one)
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// var nombre = 1;
+
+// while (nombre <= 50 ) {
+//     console.log(nombre);
+//     nombre = nombre + 1;
+// }
+
+// alert ('bienvenue dans mon programe d addition ')
+
+// var nbr1 = parseInt(prompt ('veuillez entrer votre premier nombre'))
+// while(isNaN(nbr1)) {
+//     nbr1 = parseInt(prompt('je n\'ai pas bien compris... veuillez entrer votre premier nombre'))
+// }
+
+// var nbr2 = parseInt(prompt ('veuillez entrer votre deuxième nombre')) 
+// while(isNaN(nbr2)) {
+//     nbr2 = parseInt(prompt('je n\'ai pas bien compris... veuillez entrer votre deuxième nombre'))
+// }
+
+// var resultat = (nbr1 + nbr2)
+
+// alert (nbr1 + '+' + nbr2 + '=' + resultat)
+
+// for( var repetition = 0; repetition <= 10; repetition++){
+//     console.log(repetition);
+// }
